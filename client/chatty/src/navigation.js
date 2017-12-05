8718424cf17f33142fc4b1e36916fd8cc129b729
bulotge <geoffrey.bulot@gmail.com>
2017-12-05 15:09:47 +0100

    Apply subscribeToMore to AppWithNavigationState

--- client/chatty/src/navigation.js
+++ client/chatty/src/navigation.js
@@ -1,7 +1,11 @@
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
 import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
+import { graphql, compose } from 'react-apollo';
+import update from 'immutability-helper';
+import { map } from 'lodash';
+import { Buffer } from 'buffer';
 import {
   addNavigationHelpers,
   StackNavigator,
@@ -13,6 +17,10 @@ import NewGroup from './screens/new-group';
 import FinalizeGroup from './screens/finalize-group';
 import GroupDetails from './screens/group-details';
 import Messages from './screens/messages';
+
+import { USER_QUERY } from './graphql/user.query';
+import MESSAGE_ADDED_SUBSCRIPTION from './graphql/message-added.subscription';
+import GROUP_ADDED_SUBSCRIPTION from './graphql/group-added.subscription';
 
 const styles = {
   container: {
@@ -74,6 +82,7 @@ const AppNavigator = StackNavigator({
 const firstAction = AppNavigator.router.getActionForPathAndParams('Main');
 const tempNavState = AppNavigator.router.getStateForAction(firstAction);
 const initialNavState = AppNavigator.router.getStateForAction(tempNavState);
+
 // reducer code
 export const navigationReducer = (state = initialNavState, action) => {
   let nextState;
@@ -88,17 +97,119 @@ export const navigationReducer = (state = initialNavState, action) => {
 };
 
 // Navigation component that integrates with redux
+class AppWithNavigationState extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps.user) {
+      if (this.groupSubscription) {
+        this.groupSubscription();
+      }
+      if (this.messagesSubscription) {
+        this.messagesSubscription();
+      }
+    }
+    if (nextProps.user &&
+      (!this.props.user || nextProps.user.groups.length !== this.props.user.groups.length)) {
+      // unsubscribe from old
+      if (typeof this.messagesSubscription === 'function') {
+        this.messagesSubscription();
+      }
+      // subscribe to new
+      if (nextProps.user.groups.length) {
+        this.messagesSubscription = nextProps.subscribeToMessages();
+      }
+    }
+    if (!this.groupSubscription && nextProps.user) {
+      this.groupSubscription = nextProps.subscribeToGroups();
+    }
+  }
+
+  render() {
+    const { dispatch, nav } = this.props;
+    return <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />;
+  }
+}
+
+/*
 const AppWithNavigationState = ({ dispatch, nav }) => (
   <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />
 );
+*/
 
 AppWithNavigationState.propTypes = {
   dispatch: PropTypes.func.isRequired,
   nav: PropTypes.object.isRequired,
+  subscribeToGroups: PropTypes.func,
+  subscribeToMessages: PropTypes.func,
+  user: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    email: PropTypes.string.isRequired,
+    groups: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+      }),
+    ),
+  }),
 };
 
-const MapStateToProps = state => ({
+const mapStateToProps = state => ({
   nav: state.nav,
 });
 
-export default connect(MapStateToProps, null)(AppWithNavigationState);
+const userQuery = graphql(USER_QUERY, {
+  options: () => ({ variables: { id: 1 } }), // fake the user for now
+  props: ({ data: { loading, user, subscribeToMore } }) => ({
+    loading,
+    user,
+    subscribeToMessages() {
+      return subscribeToMore({
+        document: MESSAGE_ADDED_SUBSCRIPTION,
+        variables: {
+          userId: 1, // fake the user for now
+          groupIds: map(user.groups, 'id'),
+        },
+        updateQuery: (previousResult, { subscriptionData }) => {
+          const previousGroups = previousResult.user.groups;
+          const newMessage = subscriptionData.data.messageAdded;
+          const groupIndex = map(previousGroups, 'id').indexOf(newMessage.to.id);
+          return update(previousResult, {
+            user: {
+              groups: {
+                [groupIndex]: {
+                  messages: {
+                    edges: {
+                      $set: [{
+                        __typename: 'MessageEdge',
+                        node: newMessage,
+                        cursor: Buffer.from(newMessage.id.toString()).toString('base64'),
+                      }],
+                    },
+                  },
+                },
+              },
+            },
+          });
+        },
+      });
+    },
+    subscribeToGroups() {
+      return subscribeToMore({
+        document: GROUP_ADDED_SUBSCRIPTION,
+        variables: { userId: user.id },
+        updateQuery: (previousResult, { subscriptionData }) => {
+          const newGroup = subscriptionData.data.groupAdded;
+          return update(previousResult, {
+            user: {
+              groups: { $push: [newGroup] },
+            },
+          });
+        },
+      });
+    },
+  }),
+});
+
+export default compose(
+  connect(mapStateToProps),
+  userQuery,
+)(AppWithNavigationState);

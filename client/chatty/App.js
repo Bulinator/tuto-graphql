08d6191ff59d4d7695006a1fac8c9827ed913f92
bulotge <geoffrey.bulot@gmail.com>
2018-01-17 14:29:09 +0100

    7.19 Add persistent storage

--- client/chatty/App.js
+++ client/chatty/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  AsyncStorage,
+} from 'react-native';
 import { ApolloProvider } from 'react-apollo';
 import {
   createStore,
@@ -8,7 +11,10 @@ import {
 import { composeWithDevTools } from 'redux-devtools-extension';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import thunk from 'redux-thunk';
 import AppWithNavigationState, { navigationReducer } from './src/navigation';
+import auth from './src/reducers/auth.reducer';
 
 const GRAPHQL_URL = '192.168.1.3:8080';
 // graphql endpointURL
@@ -33,12 +39,22 @@ const store = createStore(
   combineReducers({
     apollo: client.reducer(),
     nav: navigationReducer,
+    auth,
   }),
   {}, // initial state
-  composeWithDevTools(applyMiddleware(client.middleware())),
+  composeWithDevTools(
+    applyMiddleware(client.middleware(), thunk),
+    autoRehydrate(),
+  ),
 );
 
-export default class App extends React.Component {
+// Persistent storage
+persistStore(store, {
+  storage: AsyncStorage,
+  blacklist: ['apollo', 'nav'], // do not persist apollo or nav for now
+});
+
+export default class App extends Component {
   render() {
     return (
       <ApolloProvider store={store} client={client}>

e86c0d00117667c4f8fb80b66783703fea52a385
bulotge <geoffrey.bulot@gmail.com>
2018-01-18 10:19:21 +0100

    7.27 Add afterware for responses

--- client/chatty/App.js
+++ client/chatty/App.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import {
   AsyncStorage,
@@ -15,10 +16,56 @@ import { persistStore, autoRehydrate } from 'redux-persist';
 import thunk from 'redux-thunk';
 import AppWithNavigationState, { navigationReducer } from './src/navigation';
 import auth from './src/reducers/auth.reducer';
+import { logout } from './src/actions/auth.actions';
 
 const GRAPHQL_URL = '192.168.1.3:8080';
 // graphql endpointURL
 const networkInterface = createNetworkInterface({ uri: `http://${GRAPHQL_URL}/graphql` });
+
+// middleware for requests
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {};
+    }
+    // get the autentication token from local storage if it exists
+    // Before every request, we get the JWT from auth and stick it in the header.
+    // We can also run middleware after receiving responses to check
+    // for auth errors and log out the user if necessary
+    const jwt = store.getState().auth.jwt;
+    if (jwt) {
+      req.options.headers.authorization = `Bearer ${jwt}`;
+    }
+    next();
+  },
+}]);
+
+networkInterface.useAfter([{
+  applyAfterware({ response }, next) {
+    if (!response.ok) {
+      response.clone().text().then((bodyText) => {
+        console.log(`Network Error: ${response.status} (${response.statusText}) - ${bodyText}`);
+        next();
+      });
+    } else {
+      let isUnauthorized = false;
+      response.clone().json().then(({ errors }) => {
+        if (errors) {
+          console.log('GraphQL Errors:', errors);
+          if (_.some(errors, { message: 'Unauthorized' })) {
+            isUnauthorized = true;
+          }
+        }
+      }).then(() => {
+        if (isUnauthorized) {
+          store.dispatch(logout());
+        }
+        next();
+      });
+    }
+  }
+}]);
+
 export const wsClient = new SubscriptionClient(`ws://${GRAPHQL_URL}/subscriptions`, {
   reconnect: true,
   connectionParams: {
